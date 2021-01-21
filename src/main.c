30cce86a686d512c0260fffb07dfe6252e2713ab
hugues <hlarrive@laas.fr>
2021-01-21 15:00:31 +0100

    half_bridge: writing a simple test in main.c from Luiz's main.cpp (build fail)

--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@
  */
 
 /**
- * @brief       Test for OwnTech's high resolution timer driver integration in pwm_switch_driver.c
+ * @brief       Test for OwnTech's hrtim driver integration in
+ *              half_bridge.cpp
  *
  * @author      Hugues Larrive <hugues.larrive@laas.fr>
  */
@@ -16,72 +17,38 @@
 #include <devicetree.h>
 #include <drivers/gpio.h>
 
-#include "pwm_switch.h"         // PWM charge controller
+#include "half_bridge.h"        // PWM generation for DC/DC converter
+
 
 void main(void)
 {
-	printf("Libre Solar Charge Controller: %s\n", CONFIG_BOARD);
+	printf("\
+Libre Solar Charge Controller: %s\
+\n", CONFIG_BOARD);
 
     puts("\n\
- ____________________________________________________________________________________\n\
-|                                                                                    |\n\
-| Test for OwnTech's high resolution timer driver integration in pwm_switch_driver.c |\n\
-|____________________________________________________________________________________|\n\n");
+ ______________________________________________________________________\n\
+|                                                                      |\n\
+|              Test for OwnTech's high resolution timer                |\n\
+|                driver integration in half_bridge.cpp                 |\n\
+|______________________________________________________________________|\n\
+\n");
 
-    puts("! TIM3_IRQHandler() is not yet implemented\n");
+    puts("\
+//  void half_bridge_init(  print freq_kHz, int deadtime_ns,\n\
+//                          float min_duty, float max_duty  );\n\
+    half_bridge_init(200000, 100, 0.1, 0.9);\n");
+    half_bridge_init(200000, 100, 0.1, 0.9);
 
-    puts("* pwm_signal_init_registers()");
-    puts("\tpwm_signal_init_registers(200000);  // 200KHz, resolution: 11520\n");
-    pwm_signal_init_registers(200000);  // 200KHz, resolution: 11520
+    puts("\
+//  void half_bridge_set_duty_cycle(float duty);\n\
+    half_bridge_set_duty_cycle(0.5);\n");
+    half_bridge_set_duty_cycle(0.5);
 
-    puts("* pwm_signal_set_duty_cycle() will be called by pwm_signal_start()\n");
+    puts("\
+//  void half_bridge_start();\n\
+    half_bridge_start();\n");
+    half_bridge_start();
 
-    puts("* pwm_signal_duty_cycle_step()");
-    puts("\tpwm_signal_duty_cycle_step(5 * 11520 / 100);    // +5%\n");
-    pwm_signal_duty_cycle_step(5 * 11520 / 100);    // +5%
-
-    puts("* pwm_signal_get_duty_cycle()");
-    puts("\tfloat duty_cycle = pwm_signal_get_duty_cycle();");
-    float duty_cycle = pwm_signal_get_duty_cycle();
-    printf("\t> duty_cycle: %f\n\n", duty_cycle);
-
-    puts("* pwm_signal_start()");
-    puts("\tpwm_signal_start(0.5);\n");
-    pwm_signal_start(0.5);
-
-    puts("\tduty_cycle = pwm_signal_get_duty_cycle();");
-    duty_cycle = pwm_signal_get_duty_cycle();
-    printf("\t> duty_cycle: %f\n\n", duty_cycle);
-
-    puts("\tpwm_signal_duty_cycle_step(5 * 11520 / 100);    // +5%");
-    pwm_signal_duty_cycle_step(5 * 11520 / 100);    // +5%
-
-    puts("\tduty_cycle = pwm_signal_get_duty_cycle();");
-    duty_cycle = pwm_signal_get_duty_cycle();
-    printf("\t> duty_cycle: %f\n\n", duty_cycle);
-
-    puts("*** You must observe a complementary pwm signal of 200 KHz with a duty cycle of 55% on \
-PA8 and PB13");
-    puts("!!! not working on PB13, PB12 is ok using TIMC OUT1.");
-    puts("\tk_msleep(10000);\n");
-    k_msleep(10000);
-
-    puts("* pwm_signal_stop() will be tested after pwm_active()\n");
-
-    puts("! pwm_signal_high() is not yet implemented\n");
-
-    puts("* pwm_active()");
-    puts("\tbool active = pwm_active();");
-    bool active = pwm_active();
-    printf("\t> active: %u\n\n", active);
-
-    puts("* pwm_signal_stop()");
-    puts("\tpwm_signal_stop();\n");
-    pwm_signal_stop();
-    
-    puts("\tactive = pwm_active();");
-    active = pwm_active();
-    printf("\t> active: %u\n\n", active);
-    
     puts("*** the end ***\n");
 }
